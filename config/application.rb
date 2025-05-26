0b83040a7252152bf78f21f09b16354edd98b8ad
Bodacious <gavin@gavinmorrice.com>
2025-05-26 14:45:44 +0100

    Remove unncessary Rails libs

--- config/application.rb
+++ config/application.rb
@@ -1,26 +1,6 @@
 require_relative "boot"
 
-rails_libs = %w[
-  active_record/railtie
-  active_storage/engine
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  active_job/railtie
-  action_cable/engine
-  action_mailbox/engine
-  action_text/engine
-  rails/test_unit/railtie
-]
-
-applicable_libs = %w[
-  active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-  rails/test_unit/railtie
-]
-
-(rails_libs & applicable_libs).each { require _1 }
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
